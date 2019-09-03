2f34dc6dbe25f80c8fff071e31ba53aed5793d23
zalachenka <saliou.armony@gmail.com>
2019-09-03 17:05:03 +0200

    longest string function done

--- training.js
+++ training.js
@@ -3,3 +3,16 @@ function largestNumber() {
 	return Math.max(11,-2,23,4);
 };
 console.log(largestNumber());
+
+// Longest string function
+function longestString() {
+	var x = '2341312134'; 
+    var y = '@#$%^&**((*&^'; 
+  if (x.length > y.length) {
+  	return "x is a longer string";
+  }
+  else {
+  	return "y is a longer string";
+  };
+};
+console.log(longestString());
